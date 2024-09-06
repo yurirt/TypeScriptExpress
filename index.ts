c8f4b7d1b7f9734e5a8e71cb1273e095254127a9
Yuri Rojas Trujillo <yurirt.2@gmail.com>
2024-09-06 17:49:56 -0500

    work with typescript instead javascript

--- index.ts
+++ index.ts
@@ -1,8 +1,14 @@
-const express = require('express')
+import express from 'express';
+
+// const express = require('express')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
+  res.status(201).json({
+    ok: true,
+    msg: 'Nuevo Id: x'
+  });
 //   res.send('Hello World!')
 res.json({
     ok: true,
